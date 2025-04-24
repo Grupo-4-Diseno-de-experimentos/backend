ff6c8c6e86734f683ffe707067fa766af908e6a4
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2025-04-24 00:31:58 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/trabajo/Trabajo/user/domain/model/entities/User.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.trabajo.Trabajo.user.domain.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}

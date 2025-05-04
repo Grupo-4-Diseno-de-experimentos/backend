a2c19ebd4989db6d05d10a7a05124ba918faba9b
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2025-05-04 00:21:41 -0500

    feat: added Template and Food Classes

--- src/main/java/pe/edu/upc/trabajo/Trabajo/template/domain/model/aggregate/Food.java
+++ src/main/java/pe/edu/upc/trabajo/Trabajo/template/domain/model/aggregate/Food.java
@@ -1,4 +1,25 @@
 package pe.edu.upc.trabajo.Trabajo.template.domain.model.aggregate;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+@Embeddable
+@NoArgsConstructor
+@Table(name = "food")
+@Getter
+@Setter
 public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDateTime time;
+    private String calories;
+
 }
--- src/main/java/pe/edu/upc/trabajo/Trabajo/template/domain/model/entities/Template.java
+++ src/main/java/pe/edu/upc/trabajo/Trabajo/template/domain/model/entities/Template.java
@@ -1,4 +1,24 @@
 package pe.edu.upc.trabajo.Trabajo.template.domain.model.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pe.edu.upc.trabajo.Trabajo.template.domain.model.aggregate.Food;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "Template")
+@Getter
+@Setter
 public class Template {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Food food;
+
 }
